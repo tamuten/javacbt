947ed304e75e8171505f05f4452d65ac66dcc670
tamuten <tamuten310@gmail.com>
2022-07-30 07:28:15 +0900

    登録画面を作成中

--- frontend/src/Register.tsx
+++ frontend/src/Register.tsx
@@ -1,13 +1,21 @@
-import { Grid, TextField, Typography } from "@material-ui/core";
+import { Button, Grid, TextField, Typography } from "@material-ui/core";
+import { DatePicker } from "@material-ui/pickers";
 
 export const Register = () => {
   return (
     <>
-      <Typography variant="h3">Register</Typography>
+      <Typography variant="h3" gutterBottom>Register</Typography>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <TextField multiline label="automaticThinking" />
+          {/* <DatePicker value={new Date()} label="日時" disableFuture onChange={() => console.log("date changed.")} /> */}
         </Grid>
+        <Grid item xs={12}>
+          <TextField multiline fullWidth label="自動思考" />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField multiline fullWidth label="根拠" />
+        </Grid>
+        <Button>登録</Button>
       </Grid>
     </>
   );

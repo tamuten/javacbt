7252ab517a96ed52ac885a8ed25d416c359a47b7
tamuten <tamuten310@gmail.com>
2022-10-20 23:12:05 +0900

    Material-iconsを導入

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
-import { AppBar, Button, Container, Stack, Toolbar, Typography } from '@mui/material';
+import { AppBar, Button, Container, Drawer, IconButton, Stack, Toolbar, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Detail } from './Detail';
 import { Register } from './Register';
 import { ThoughtList } from './ThoughtList';
 
+const drawerWidth = 240;
+
 export const App = () => {
+    const [drowerOpen, setDrowerOpen] = useState(false);
+
+    const handleDrawerOpen = () => {
+        setDrowerOpen(open => !open);
+    }
+
+    const toggleOpen = () => {
+        setDrowerOpen(!drowerOpen);
+    }
 
     return (
         <>
@@ -12,6 +25,14 @@ export const App = () => {
                 <AppBar position="static">
                     <Toolbar>
                         <Stack direction="row" spacing={3}>
+                            <IconButton
+                                color="inherit"
+                                aria-label="open drawer"
+                                onClick={handleDrawerOpen}
+                                edge="start"
+                            >
+                                <MenuIcon />
+                            </IconButton>
                             <Typography variant="h6">Cbt App</Typography>
                             <Button href="/" color="inherit">List</Button>
                             <Button href="/register" color="inherit">Register</Button>
@@ -20,6 +41,21 @@ export const App = () => {
                     </Toolbar>
 
                 </AppBar>
+                <Drawer
+                    sx={{
+                        width: drawerWidth,
+                        flexShrink: 0,
+                        '& .MuiDrawer-paper': {
+                            width: drawerWidth,
+                            boxSizing: 'border-box',
+                        }
+                    }}
+                    anchor="left"
+                    open={drowerOpen}
+                    onClose={toggleOpen}
+                >
+                    <Toolbar />
+                </Drawer>
                 <Container maxWidth="lg">
                     <div style={{ padding: 30 }}>
                         <Routes>
